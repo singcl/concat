9a9394efbcf086a4e8ffccae0d609f4a9489a70e
singcl <24661881@qq.com>
2018-05-08 14:02:25 +0800

    :art: update test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 var targetDir = './txt'
-var createJSON = './data'
+var dest = './data'
 
 var concat = require('./concat')
 
-concat(targetDir, createJSON, function(err) {
+concat(targetDir, dest, function(err) {
     if (err) return console.error('ERROR:', err)
     console.log('完成')
 })
